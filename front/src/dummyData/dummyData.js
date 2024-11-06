ef6bfaefc853d091d4e08bc67873128da126bcac
hyeonjin6530 <cola1876@naver.com>
2024-11-06 20:36:31 +0900

    test: 테스트를 위한 더미데이터 생성

---
+++ front/src/dummyData/dummyData.js
@@ -0,0 +1,100 @@
+const dummyProjects = [
+  {
+    projectId: '1',
+    title: '프로젝트 1',
+    description: '첫 번째 예시 프로젝트입니다.',
+    createdAt: '2023-11-01',
+    tasks: [
+      {
+        taskId: '1',
+        name: '태스크 1',
+        description: '첫 번째 태스크입니다.',
+        status: '완료',
+        priority: '상',
+        dueDate: '2023-11-02',
+      },
+      {
+        taskId: '2',
+        name: '태스크 2',
+        description: '두 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '중',
+        dueDate: '2023-11-07',
+      },
+      {
+        taskId: '3',
+        name: '태스크 3',
+        description: '세 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '하',
+        dueDate: '2023-11-10',
+      },
+    ],
+  },
+  {
+    projectId: '2',
+    title: '프로젝트 2',
+    description: '첫 번째 예시 프로젝트입니다.',
+    createdAt: '2023-10-20',
+    tasks: [
+      {
+        taskId: '1',
+        name: '태스크 1',
+        description: '첫 번째 태스크입니다.',
+        status: '완료',
+        priority: '상',
+        dueDate: '2023-11-02',
+      },
+      {
+        taskId: '2',
+        name: '태스크 2',
+        description: '두 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '중',
+        dueDate: '2023-11-07',
+      },
+      {
+        taskId: '3',
+        name: '태스크 3',
+        description: '세 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '하',
+        dueDate: '2023-11-10',
+      },
+    ],
+  },
+  {
+    projectId: '3',
+    title: '프로젝트 3',
+    description: '첫 번째 예시 프로젝트입니다.',
+    createdAt: '2023-09-15',
+    tasks: [
+      {
+        taskId: '1',
+        name: '태스크 1',
+        description: '첫 번째 태스크입니다.',
+        status: '완료',
+        priority: '상',
+        dueDate: '2023-11-02',
+      },
+      {
+        taskId: '2',
+        name: '태스크 2',
+        description: '두 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '중',
+        dueDate: '2023-11-07',
+      },
+      {
+        taskId: '3',
+        name: '태스크 3',
+        description: '세 번째 태스크입니다.',
+        status: '진행 중',
+        priority: '하',
+        dueDate: '2023-11-10',
+      },
+    ],
+  },
+];
+
+export default dummyProjects;
