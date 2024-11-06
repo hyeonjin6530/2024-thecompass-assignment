b9e2b25a35cf8515e412d3bbb4335774c4480d57
hyeonjin6530 <cola1876@naver.com>
2024-11-06 19:37:02 +0900

    chore: 페이지 연결 및 설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import MainPage from './pages/mainPage/MainPage';
+import ProjectDetailPage from './pages/projectDetailPage/ProjectDetailPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>하이하이</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route
+            path="/detailpage/:projectId"
+            element={<ProjectDetailPage />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
