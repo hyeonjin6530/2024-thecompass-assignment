07365c61f613db849b89da1ebebd37e0af7873c0
hyeonjin6530 <cola1876@naver.com>
2024-11-06 10:40:03 +0900

    fix: 결과가 여러번 출력되는 오류 수정

--- common/second-algorithm.js
+++ common/second-algorithm.js
@@ -1,34 +1,32 @@
 function isEmpty(value) {
     if (value === null || value === undefined) {
-        return console.log(true);
+        return true;
     }
 
     if (typeof value !== 'object') {
-        return console.log(false);
+        return false;
     }
 
     if (Array.isArray(value)) {
-        const result = value.length === 0 || value.every(isEmpty);
-        return console.log(result);
+        return value.length === 0 || value.every(isEmpty);
     }
 
     if (typeof value === 'object') {
-        const result = Object.keys(value).length === 0 || Object.values(value).every(isEmpty);
-        return console.log(result);
+        return Object.keys(value).length === 0 || Object.values(value).every(isEmpty);
     }
 
-    return console.log(false);
+    return false;
 
 }
 
-isEmpty(null); // 출력: true
+console.log(isEmpty(null)); // 출력: true
 
-isEmpty({}); // 출력: true
+console.log(isEmpty({})); // 출력: true
 
-isEmpty(0); // 출력: false
+console.log(isEmpty(0)); // 출력: false
 
-isEmpty(false); // 출력: false
+console.log(isEmpty(false)); // 출력: false
 
-isEmpty([{}, {a:[]}]); // 출력: true
+console.log(isEmpty([{}, {a:[]}])); // 출력: true
 
-isEmpty({a: null, b: ''}); // 출력: true
+console.log(isEmpty({a: null, b: ''})); // 출력: true
